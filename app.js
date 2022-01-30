392bbb2deca11feccbd076c68564777e896e4470
Deepak Lokare <deepaklokare@Deepaks-MacBook-Air.local>
2022-01-30 20:46:28 +0530

    building RESTful APIs using node

--- app.js
+++ app.js
@@ -1,60 +1,84 @@
-const Logger = require("./logger");
-const path = require("path");
-const os = require("os");
-const fs = require("fs");
-const http = require("http");
-const _ = require("underscore");
-const logger = new Logger();
+const Joi = require("joi");
+const express = require("express");
+const app = express();
 
-console.log(__dirname);
-console.log(__filename);
+app.use(express.json()); // adding a middleware to convert request body
 
-const pathObj = path.parse(__filename);
+const courses = [
+  { id: 1, name: "Course 1" },
+  { id: 2, name: "Course 2" },
+  { id: 3, name: "Course 3" },
+];
 
-console.log(pathObj);
-
-console.log(os.totalmem());
-console.log(os.freemem());
-console.log(os.hostname());
-console.log(os.platform());
-console.log(os.release());
-console.log(os.uptime());
-
-//const files = fs.readdirSync("./");
-// console.log(files);
-
-fs.readdir("./", function (err, files) {
-  if (!err) console.log(err);
-  console.log(files);
+const schema = Joi.object({
+  name: Joi.string().min(3).required(),
 });
 
-// working with node events
-
-// Listening an event
-//emitter.addListener()
-logger.on("messageLogged", function (arg) {
-  console.log("listening to messageLogged event", arg);
+app.get("/", (req, res) => {
+  console.log("Received request");
+  res.send("Hello World");
 });
 
-logger.log("Hi dude");
-
-//--------Http Module-------
-
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.write("Hey Hi.. How you doing...?");
-    res.end();
-  }
-
-  if (req.url === "/api/courses") {
-    let courses = [1, 3, 2, 5];
-    let doesContains = _.contains(courses, 4);
-    console.log("Does courses contain id with 4? ", doesContains);
-
-    res.write(JSON.stringify([1, 2, 3]));
-    res.end();
-  }
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
 });
 
-server.listen(3000);
-console.log("Listening on port 3000...");
+app.get("/api/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const course = courses.find((c) => c.id === parseInt(id));
+  if (!course)
+    return res.status(404).send("The course with given id does not exists!");
+
+  res.send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+  const { error, value } = schema.validate(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+app.put("/api/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const course = courses.find((c) => c.id === parseInt(id));
+
+  if (!course)
+    return res.status(404).send("The course with given id does not exists!");
+
+  const { error, value } = schema.validate(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+app.delete("/api/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const index = courses.findIndex((c) => c.id === parseInt(id));
+  const course = courses[index];
+
+  if (!course)
+    return res.status(404).send("The course with given id does not exists!");
+
+  courses.splice(index, 1);
+  res.send(course);
+});
+
+app.get("/api/posts/:year/:month", (req, res) => {
+  const output = {
+    params: req.params,
+    queryStrings: req.query,
+  };
+  res.send(output);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
